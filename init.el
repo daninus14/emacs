ad51f1ca3fc3490f33f58be642b965ce35f79f4d
Daniel Nussenbaum <daninus14@gmail.com>
2023-09-27 16:44:15 +0300

    initial commit

---
+++ init.el
@@ -0,0 +1,211 @@
+; (load (expand-file-name "~/.quicklisp/slime-helper.el"))
+;(setq inferior-lisp-program "sbcl")
+
+;(slime-setup '(slime-fancy slime-quicklisp slime-asdf))
+
+
+;; For Roswell - DN
+; (load (expand-file-name "~/.roswell/helper.el"))
+; (setq inferior-lisp-program "ros -Q run")
+
+
+;; Setup load-path, autoloads and your lisp system
+;(add-to-list 'load-path "~/dir/to/cloned/slime")
+;(require 'slime-autoloads)
+
+; (require 'package)
+; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+; ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+; ;; and `package-pinned-packages`. Most users will not need or want to do this.
+; ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+; (package-initialize)
+
+
+; ;;(add-hook 'lisp-mode-hook '(lambda () 
+; ;;                              (unless (get-process "SLIME Lisp")
+; ;;                                (slime))))
+; ;;
+; ;;(add-hook 'lisp-mode-hook '(lambda ()
+; ;;                                  (unless (get-process "SLIME Lisp")
+; ;;                                     (let ((oldbuff (current-buffer)))
+; ;;                                       (slime)
+; ;;                                       (switch-buffer oldbuff))))
+
+
+; (setq show-paren-delay 0)
+; (show-paren-mode 1)
+
+; (custom-set-variables
+;  ;; custom-set-variables was added by Custom.
+;  ;; If you edit it by hand, you could mess it up, so be careful.
+;  ;; Your init file should contain only one such instance.
+;  ;; If there is more than one, they won't work right.
+;  '(package-selected-packages '(slime-volleyball)))
+; (custom-set-faces
+;  ;; custom-set-faces was added by Custom.
+;  ;; If you edit it by hand, you could mess it up, so be careful.
+;  ;; Your init file should contain only one such instance.
+;  ;; If there is more than one, they won't work right.
+;  )
+
+
+; https://github.com/benjaminor/kkp
+
+(when (>= emacs-major-version 24)
+  (progn
+    ;; load emacs 24's package system. Add MELPA repository.
+    (require 'package)
+    (add-to-list
+     'package-archives
+     '("melpa" . "https://melpa.org/packages/")
+     t))
+;  (package-refresh-contents)
+  (when (< emacs-major-version 27) (package-initialize)))
+
+
+
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(counsel ivy doom-themes exec-path-from-shell kkp slime-volleyball)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(use-package kkp
+  :ensure t
+  :config
+  ;; (setq kkp-alt-modifier 'alt) ;; use this if you want to map the Alt keyboard modifier to Alt in Emacs (and not to Meta)
+  (global-kkp-mode +1))
+
+
+;; -*- mode: elisp -*-
+
+;; Disable the splash screen (to enable it agin, replace the t with 0)
+(setq inhibit-splash-screen t)
+
+;; Enable transient mark mode
+(transient-mark-mode 1)
+
+;;;;Org mode configuration
+;; Enable Org mode
+(require 'org)
+;; Make Org mode work with files ending in .org
+;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+;; The above is the default in recent emacsen
+
+(defun xah-copy-line-or-region ()
+  "Copy current line or selection.
+When called repeatedly, append copy subsequent lines.
+When `universal-argument' is called first, copy whole buffer (respects `narrow-to-region').
+
+URL `http://xahlee.info/emacs/emacs/emacs_copy_cut_current_line.html'
+Version: 2010-05-21 2022-10-03"
+  (interactive)
+  (let ((inhibit-field-text-motion nil))
+    (if current-prefix-arg
+        (progn
+          (copy-region-as-kill (point-min) (point-max)))
+      (if (region-active-p)
+          (progn
+            (copy-region-as-kill (region-beginning) (region-end)))
+        (if (eq last-command this-command)
+            (if (eobp)
+                (progn )
+              (progn
+                (kill-append "\n" nil)
+                (kill-append
+                 (buffer-substring-no-properties (line-beginning-position) (line-end-position))
+                 nil)
+                (progn
+                  (end-of-line)
+                  (forward-char))))
+          (if (eobp)
+              (if (eq (char-before) 10 )
+                  (progn )
+                (progn
+                  (copy-region-as-kill (line-beginning-position) (line-end-position))
+                  (end-of-line)))
+            (progn
+              (copy-region-as-kill (line-beginning-position) (line-end-position))
+              (end-of-line)
+              (forward-char))))))))
+
+(defun xah-cut-line-or-region ()
+  "Cut current line or selection.
+When `universal-argument' is called first, cut whole buffer (respects `narrow-to-region').
+
+URL `http://xahlee.info/emacs/emacs/emacs_copy_cut_current_line.html'
+Version: 2010-05-21 2015-06-10"
+  (interactive)
+  (if current-prefix-arg
+      (progn ; not using kill-region because we don't want to include previous kill
+        (kill-new (buffer-string))
+        (delete-region (point-min) (point-max)))
+    (progn (if (region-active-p)
+               (kill-region (region-beginning) (region-end) t)
+             (kill-region (line-beginning-position) (line-beginning-position 2))))))
+
+
+(global-set-key (kbd "<f2>") 'xah-cut-line-or-region) ; cut
+(global-set-key (kbd "<f3>") 'xah-copy-line-or-region) ; copy
+(global-set-key (kbd "<f4>") 'yank) ; paste
+
+; figure out how to enable it only in org mode??
+;(add-hook 'text-mode-hook 'visual-line-mode)
+
+(visual-line-mode)
+
+(desktop-save-mode 1)
+
+
+;; https://www.juniordeveloperdiaries.com/emacs-intro/ is the next section
+
+(setq
+ use-package-always-ensure t ;; Makes sure to download new packages if they aren't already downloaded
+ use-package-verbose t) ;; Package install logging. Packages break, it's nice to know why.
+
+;; Slurp environment variables from the shell.
+;; a.k.a. The Most Asked Question On r/emacs
+(use-package exec-path-from-shell
+  :config
+  (exec-path-from-shell-initialize))
+
+(use-package doom-themes
+  :init
+  (load-theme 'doom-one))
+
+;; Any Customize-based settings should live in custom.el, not here.
+(setq custom-file "~/.emacs.d/custom.el") ;; Without this emacs will dump generated custom settings in this file. No bueno.
+(load custom-file 'noerror)
+
+;;; OS specific config
+(defconst *is-a-mac* (eq system-type 'darwin))
+(defconst *is-a-linux* (eq system-type 'gnu/linux))
+
+
+;; Fullscreen by default, as early as possible. This tiny window is not enough
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; Make M-x and other mini-buffers sortable, filterable
+(use-package ivy
+  :init
+  (ivy-mode 1)
+  (setq ivy-height 15
+        ivy-use-virtual-buffers t
+        ivy-use-selectable-prompt t))
+
+(use-package counsel
+  :after ivy
+  :init
+  (counsel-mode 1)
+  :bind (:map ivy-minibuffer-map))
